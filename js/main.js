b18cf66207cd30e434fc878f9481cc815622cd74
Nadhir <nadhirxz@gmail.com>
2022-06-24 18:14:47 +0100

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,258 @@
+var Base64 = {
+
+    // private property
+    _keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="
+
+    // public method for encoding
+    , encode: function (input)
+    {
+        var output = "";
+        var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+        var i = 0;
+
+        input = Base64._utf8_encode(input);
+
+        while (i < input.length)
+        {
+            chr1 = input.charCodeAt(i++);
+            chr2 = input.charCodeAt(i++);
+            chr3 = input.charCodeAt(i++);
+
+            enc1 = chr1 >> 2;
+            enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+            enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+            enc4 = chr3 & 63;
+
+            if (isNaN(chr2))
+            {
+                enc3 = enc4 = 64;
+            }
+            else if (isNaN(chr3))
+            {
+                enc4 = 64;
+            }
+
+            output = output +
+                this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
+                this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
+        } // Whend 
+
+        return output;
+    } // End Function encode 
+
+
+    // public method for decoding
+    ,decode: function (input)
+    {
+        var output = "";
+        var chr1, chr2, chr3;
+        var enc1, enc2, enc3, enc4;
+        var i = 0;
+
+        input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+        while (i < input.length)
+        {
+            enc1 = this._keyStr.indexOf(input.charAt(i++));
+            enc2 = this._keyStr.indexOf(input.charAt(i++));
+            enc3 = this._keyStr.indexOf(input.charAt(i++));
+            enc4 = this._keyStr.indexOf(input.charAt(i++));
+
+            chr1 = (enc1 << 2) | (enc2 >> 4);
+            chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+            chr3 = ((enc3 & 3) << 6) | enc4;
+
+            output = output + String.fromCharCode(chr1);
+
+            if (enc3 != 64)
+            {
+                output = output + String.fromCharCode(chr2);
+            }
+
+            if (enc4 != 64)
+            {
+                output = output + String.fromCharCode(chr3);
+            }
+
+        } // Whend 
+
+        output = Base64._utf8_decode(output);
+
+        return output;
+    } // End Function decode 
+
+
+    // private method for UTF-8 encoding
+    ,_utf8_encode: function (string)
+    {
+        var utftext = "";
+        string = string.replace(/\r\n/g, "\n");
+
+        for (var n = 0; n < string.length; n++)
+        {
+            var c = string.charCodeAt(n);
+
+            if (c < 128)
+            {
+                utftext += String.fromCharCode(c);
+            }
+            else if ((c > 127) && (c < 2048))
+            {
+                utftext += String.fromCharCode((c >> 6) | 192);
+                utftext += String.fromCharCode((c & 63) | 128);
+            }
+            else
+            {
+                utftext += String.fromCharCode((c >> 12) | 224);
+                utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+                utftext += String.fromCharCode((c & 63) | 128);
+            }
+
+        } // Next n 
+
+        return utftext;
+    } // End Function _utf8_encode 
+
+    // private method for UTF-8 decoding
+    ,_utf8_decode: function (utftext)
+    {
+        var string = "";
+        var i = 0;
+        var c, c1, c2, c3;
+        c = c1 = c2 = 0;
+
+        while (i < utftext.length)
+        {
+            c = utftext.charCodeAt(i);
+
+            if (c < 128)
+            {
+                string += String.fromCharCode(c);
+                i++;
+            }
+            else if ((c > 191) && (c < 224))
+            {
+                c2 = utftext.charCodeAt(i + 1);
+                string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+                i += 2;
+            }
+            else
+            {
+                c2 = utftext.charCodeAt(i + 1);
+                c3 = utftext.charCodeAt(i + 2);
+                string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+                i += 3;
+            }
+
+        } // Whend 
+
+        return string;
+    } // End Function _utf8_decode 
+
+}
+
+
+
+function encrypt() {
+    let text = Base64.encode(document.getElementById('text').value);
+    let method = document.getElementById("method").value;
+    let password = document.getElementById("password").value;
+    if (password=="") password = "urcrypter";
+    let result = "";
+    let thevariablethatimgonnauseonlyoncefuckyou = false;
+    switch(method) {
+        case "0":
+            document.getElementById("method").classList.add("is-invalid");
+        break;
+        case "1":
+            document.getElementById("method").classList.remove("is-invalid");
+            result = caesar_cipher(0,text,password);
+        break;
+        case "2":
+            document.getElementById("method").classList.remove("is-invalid");
+            if (text == password) {
+                result = "[YOUR PASSWORD SHOULD NOT MATCH THE TEXT YOU ENTERED]";
+            } else {
+                result = xor_cipher(0,text,password);
+            }
+        break;
+        case "3":
+            document.getElementById("method").classList.remove("is-invalid");
+            let isnum = /^\d+$/.test(Base64.decode(text));
+                if (isnum) {
+                    document.getElementById("text").classList.remove("is-invalid");
+                    result = sec_number_xor(0,Base64.decode(text),password);
+                } else {
+                    document.getElementById("result").classList.add("is-invalid");
+                    document.getElementById("result").value = "[PLEASE ENTER ONLY NUMBERS]";
+                    thevariablethatimgonnauseonlyoncefuckyou = true;
+                }
+        break;
+    }
+    if (!thevariablethatimgonnauseonlyoncefuckyou) {
+        document.getElementById("result").value = result;
+    }
+}
+function decrypt() {
+    let text = document.getElementById('text').value;
+    let method = document.getElementById("method").value;
+    let password = document.getElementById("password").value;
+    if (password=="") password = "urcrypter";
+    let result = "";
+    let thevariablethatimgonnauseonlyoncefuckyme = false;
+    switch(method) {
+        case "0":
+            document.getElementById("method").classList.add("is-invalid");
+            document.getElementById("invld").style.display = "block";
+        break;
+        case "1":
+            document.getElementById("method").classList.remove("is-invalid");
+            document.getElementById("invld").style.display = "none";
+            result = caesar_cipher(1,text,password);
+        break;
+        case "2":
+            document.getElementById("method").classList.remove("is-invalid");
+            if (text == password) {
+                result = "[YOUR PASSWORD SHOULD NOT MATCH THE TEXT YOU ENTERED]";
+            } else {
+                result = binToAscii(xor_cipher(1,text,password));
+            }
+        break;
+        case "3":
+            document.getElementById("method").classList.remove("is-invalid");
+            result = sec_number_xor(1,text,password);
+            thevariablethatimgonnauseonlyoncefuckyme = true;
+        break;
+    }
+    if (thevariablethatimgonnauseonlyoncefuckyme) {
+        document.getElementById("result").value = result;
+    } else {
+        document.getElementById("result").value = Base64.decode(result);
+    }
+}
+function pswrd() {
+    var x = document.getElementById("password");
+    if (x.type == "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
+
+window.onresize = function () {
+    if (window.innerWidth < 780) {
+        document.getElementById("content").style.display = "none";
+        document.getElementById("mob").style.display = "block";
+    } else {
+        document.getElementById("content").style.display = "block";
+        document.getElementById("mob").style.display = "none";
+    }
+}
+window.onload = function () {
+    if (window.innerWidth < 780) {
+        document.getElementById("content").style.display = "none";
+        document.getElementById("mob").style.display = "block";
+    } else {
+        document.getElementById("content").style.display = "block";
+        document.getElementById("mob").style.display = "none";
+    }
+}
